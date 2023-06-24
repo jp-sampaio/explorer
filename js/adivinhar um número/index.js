bccf2bc493ee24e2bf0b793a9c11d4836e8e6e35
JP Sampaio <108679561+jp-sampaio@users.noreply.github.com>
2023-06-24 19:02:53 -0300

    update desafio adivinhação

--- js/adivinhar um número/index.js
+++ js/adivinhar um número/index.js
@@ -24,7 +24,7 @@ let userNumber = prompt("Adivinhe o número que estou pensando? Está entre 0 e 
 let randomNumber = Math.round(Math.random() * 10);
 
 // 3° passo - Verificar se o número digitado é o mesmo que o aleatório gerado pelo sistema.
-let match = Number(userNumber) !== randomNumber;
+// let match = Number(userNumber) !== randomNumber;
 
 // 4° passo - Enquanto o usuário não adivinhar o número, mostrar a mensagem: "Erro, tente novamente:"
 
@@ -32,7 +32,7 @@ let match = Number(userNumber) !== randomNumber;
 let xAttempts = 1;
 
 // Enquanto a condição for verdadeira vai rodar a expressão. Se a condição vier true como estou negando vai ser false e para a aplicação, agora se vinher false vai ser true e continuar;
-while (match) {
+while (Number(userNumber) !== randomNumber) {
   userNumber = prompt("Erro, tente novamente:");
   xAttempts++;
 
